d901b891bac75ef921d0831d36fcf1e62262173f
ruoxijiang <rjiang@lazycakestudios.net>
2020-08-17 14:17:47 +0800

    Update tnef lib

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-var tnef = require('bindings')('tnef.node');
-
-console.log(tnef.ExtractFiles("/Users/gtkrab/Downloads/winmail.dat","/Users/gtkrab/Downloads/"));
+const tnef = require('bindings')('tnef.node');
+function extractFiles(file_path, output_dir){
+    return new Promise((resolve, reject) => {
+        if(typeof file_path !== 'string' || file_path.length === 0){
+            throw new Error(`file path not present`);
+        }
+        if(typeof output_dir !== 'string' || file_path.length === 0){
+            throw new Error(`file path not present`);
+        }
+        const ret = tnef.ExtractFiles(file_path, output_dir);
+        if(ret){
+            resolve(ret);
+        }else {
+            reject();
+        }
+    });
+}
+extractFiles('/PathTo/winmail.dat', '/PathTo/').then(ret=>{
+    console.log(ret[0].size);
+});
+module.exports = {
+    extractFiles: extractFiles,
+}
--- src/tnef.cc
+++ src/tnef.cc
@@ -1,6 +1,7 @@
 #include "napi.h"
 extern "C" {
 #include "vendor/tnef/src/node_native.h"
+#include "vendor/tnef/src/tnef.h"
 }
 namespace {
     Napi::Value ExtractFiles(const Napi::CallbackInfo &info) {
@@ -15,10 +16,29 @@ namespace {
                     ThrowAsJavaScriptException();
             return env.Null();
         }
+        File_List fileList;
+        File_List tmp;
         const std::string file_name = info[0].As<Napi::String>();
         const std::string dest = info[1].As<Napi::String>();
-        extract_files(file_name.c_str(), dest.c_str());
-        return Napi::String::New(env, dest);
+        extract_files(file_name.c_str(), dest.c_str(), &fileList);
+        Napi::Array ret = Napi::Array::New(env);
+        int i =0;
+        tmp = fileList;
+            while(tmp != NULL) {
+//                    Napi::Object fileInfo = Napi::Object::New(env);
+//                fileInfo.Set(Napi::String::New(env, "name"), Napi::String::New(env, tmp->info.file_name));
+//                fileInfo.Set(Napi::String::New(env, "path"), Napi::String::New(env, tmp->info.path));
+//                fileInfo.Set(Napi::String::New(env, "contentType"),
+//                            Napi::String::New(env, tmp->info.mime_type));
+//                fileInfo.Set(Napi::String::New(env, "contentId"),
+//                            Napi::String::New(env, tmp->info.content_id));
+//                fileInfo.Set(Napi::String::New(env, "size"), Napi::Number::New(env, tmp->info.len));
+//                    ret.Set(i, fileInfo);
+                    tmp = tmp->next;
+                i++;
+            }
+            free_file_list(&fileList);
+        return ret;
     }
 
     Napi::Value GetFilesInfo(const Napi::CallbackInfo &info) {
