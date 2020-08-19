2197cd8ba20ba0dca48b79d05b1dfe61d0fe979d
ruoxijiang <rjiang@lazycakestudios.net>
2020-08-19 09:54:52 +0800

    Update install process

--- tnef-configure.js
+++ tnef-configure.js
@@ -1,9 +1,9 @@
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
 const path = require('path');
 const gitPath = path.join(__dirname, 'src', 'vendor');
 const pwd = path.join(gitPath, 'tnef');
 function runShell(cmd,args,pwd, cb, stage){
-    const shell = spawn(cmd, args, {cwd: pwd, env: process.env, shell: true});
+    const shell = exec(cmd, args, {cwd: pwd, env: process.env, shell: '/bin/bash'});
     shell.stdout.on('data', data => {
         console.log(`${stage}:std: ${data}`);
     });
@@ -19,7 +19,7 @@ function runShell(cmd,args,pwd, cb, stage){
     shell.on('error', data=>{
         console.log(`${stage}:error ${data}`);
     })
-}
+};
 function runNodeGyp(){
     runShell('node-gyp', ['configure'], __dirname, null, 'node-gyp configure');
 }
